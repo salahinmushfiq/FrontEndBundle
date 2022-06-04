22de8decbd9b9ad57e5aa3671695d800296ca39f
salahinmushfiq <salahinmushfiq@gmail.com>
2022-06-04 10:58:32 +0600

    scroll function updated

--- Eternal/main.js
+++ Eternal/main.js
@@ -126,7 +126,7 @@ function scrollFunction() {
         }
     }else{
         if(document.body.scrollTop > 70 || document.documentElement.scrollTop > 70){
-            document.getElementById("nav-wrapper").style.background = "black";
+            document.getElementById("nav-wrapper").style.background = "transparent";
             document.getElementById("nav-links").style.background = "black";
             document.getElementById("nav").style.background = "black";
             document.getElementById("nav").style.height = "6vh+5px";
